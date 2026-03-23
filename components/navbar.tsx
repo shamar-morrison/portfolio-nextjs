cd5a4b50c40b2768eff84e4086b2bc174705a606
Shamar Morrison <shamar.webdev@gmail.com>
2026-03-23 04:28:05 -0500

    chore: Update resume Google Drive ID.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false)
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-  const RESUME_ID = "1zu5bFkcpGQEf51dMGEpPX34jIKemqYat" // Google Drive File ID
+  const RESUME_ID = "13mORJ50BSa8fZZTySLjoGB90fR-qTMun" // Google Drive File ID
   const CVLink = `https://drive.google.com/uc?export=download&id=${RESUME_ID}`
 
   useEffect(() => {
