ce610e8124b6ba3c4128dcb5d63815a5dc30fb55
Shamar Morrison <shamar.webdev@gmail.com>
2026-08-20 01:05:07 -0500

    style: update navbar indicator color logic to maintain slate-600 when inactive

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -45,8 +45,8 @@ const Navbar = () => {
       >
         <span
           aria-hidden="true"
-          className={`mr-4 h-px bg-slate-600 transition-all motion-reduce:transition-none group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 ${
-            isActive ? "w-16 bg-slate-200" : "w-8"
+          className={`mr-4 h-px transition-all motion-reduce:transition-none group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 ${
+            isActive ? "w-16 bg-slate-200" : "w-8 bg-slate-600"
           }`}
         />
         <span
